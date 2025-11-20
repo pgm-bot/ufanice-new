849936f6e230e35e63cfe8f5a01e7dc774bf602e
jirawutt <jirawutt@hotmail.com>
2025-11-21 02:21:01 +0700

    Update Page Card

--- app/pages/card/index.vue
+++ app/pages/card/index.vue
@@ -1,4 +1,6 @@
 <template>
+  <HeadMobile></HeadMobile>
+  <Header></Header>
   <div class="contentbody">
     <div id="section-content" class="container mt-0">
       <div class="bar-back">
@@ -6,13 +8,9 @@
           <i class="fas fa-chevron-left" aria-hidden="true"></i> หน้าหลัก
         </nuxt-link>
       </div>
-      <div
-        class="p-2 w-100 bg-light main-content align-self-stretch"
-        style="min-height: calc((100vh - 106px) - 90px);"
-      >
+      <div class="p-2 w-100 bg-light main-content align-self-stretch" style="min-height: calc((100vh - 106px) - 90px);">
         <div
-          class="bgwhitealpha text-secondary shadow-sm rounded p-2 xtarget col-lotto d-flex flex-row justify-content-between mb-1 pb-0"
-        >
+          class="bgwhitealpha text-secondary shadow-sm rounded p-2 xtarget col-lotto d-flex flex-row justify-content-between mb-1 pb-0">
           <div class="lotto-title">
             <h4>
               <i class="fas fa-gift" aria-hidden="true"></i>
@@ -20,20 +18,14 @@
             </h4>
           </div>
         </div>
-        <div
-          class="bgwhitealpha text-secondary shadow-sm rounded pl-3 pr-3 pt-2 pb-2 col-lotto text-left"
-        >
+        <div class="bgwhitealpha text-secondary shadow-sm rounded pl-3 pr-3 pt-2 pb-2 col-lotto text-left">
           <div class="user-dashboard-tab">
             <div class="row">
               <div class="order-details col-12">
                 <div class="justify-content-center">
                   <div class="text-center">
-                    <img
-                      @click="$router.push('/card/game')"
-                      src="/ads/11.jpg"
-                      alt="เปิดไพ่เสี่ยงโชค"
-                      style="width:100%;cursor:pointer"
-                    />
+                    <img @click="$router.push('/card/game')" src="/ads/11.jpg" alt="เปิดไพ่เสี่ยงโชค"
+                      style="width:100%;cursor:pointer" />
                   </div>
 
                   <p class="mt-3">
@@ -41,15 +33,14 @@
                     <br />- ต้องมียอดฝากแรกของวันขั้นต่ำ 100 บาท
                     <br />- เล่นได้วันละครั้งเท่านั้น..รีระบบหลัง 00.00 ของทุกวัน
                     <br />- แจกวันละ 5000 user เท่านั้น! รับได้อีกทีหลังเวลารีระบบ
-<!--                    <br />- ต้องมีเงินคงเหลือไม่ต่ำกว่า 100 บาท จึงจะสามารถเปิดได้-->
-                    <br />- เมื่อรับโบนัสเกมไพ่แล้ว จะต้องทำเทิน 4 เท่าของ โบนัสที่เปิดได้ ถึงจะถอนเงินได้ หากท่านรับโบนัสอื่นอยู่จะเปิดไพ่ไม่ได้
+                    <!--                    <br />- ต้องมีเงินคงเหลือไม่ต่ำกว่า 100 บาท จึงจะสามารถเปิดได้-->
+                    <br />- เมื่อรับโบนัสเกมไพ่แล้ว จะต้องทำเทิน 4 เท่าของ โบนัสที่เปิดได้ ถึงจะถอนเงินได้
+                    หากท่านรับโบนัสอื่นอยู่จะเปิดไพ่ไม่ได้
                   </p>
                   <!-- <br /> -->
                   <h2>
-                    <nuxt-link
-                      class="text-red raw raw-open"
-                      to="/card/game"
-                    >เริ่มเปิดไพ่เสี่ยงโชค คลิกที่นี้!</nuxt-link>
+                    <nuxt-link class="text-red raw raw-open" to="/card/game">เริ่มเปิดไพ่เสี่ยงโชค
+                      คลิกที่นี้!</nuxt-link>
                   </h2>
                 </div>
               </div>
@@ -59,6 +50,7 @@
       </div>
     </div>
   </div>
+   <FixedFooter></FixedFooter>
 </template>
 
 <script>
@@ -86,6 +78,7 @@ export default {
 .text-red {
   color: red !important;
 }
+
 .text-red:hover {
   color: rgb(153, 11, 11) !important;
 }
@@ -100,10 +93,12 @@ h2 {
     font-size: 18px !important;
   }
 }
+
 @media (max-width: 576px) {
   .lotto-title h4 {
     font-size: 18px !important;
   }
+
   /* .text-detail {
     font-size: 16px !important;
   } */
